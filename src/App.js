1f6d49b58697191b430d4d904bf188b647057ae6
Mrunmayi Dharmadhikari <mrunmayi016@gmail.com>
2021-05-13 00:59:29 -0700

    Added styles to tasklist layout

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
-import React, {useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  
   const [tasklist, setTaskList] = useState([]);
-  
+
   const inputRef = useRef();
 
   const keyPressHandler = (event) => {
-      if(event.key==="Enter") {
-        addTask();
-      } 
-  }
+    if (event.key === "Enter") {
+      addTask();
+    }
+  };
 
   const addTask = () => {
     const newTask = inputRef.current.value;
     tasklist.push(newTask);
-    inputRef.current.value = '';
+    inputRef.current.value ='';
     setTaskList([...tasklist]);
+  };
+
+  const listMapper = (list) => {
+    return(
+      <li> {list} </li>
+    )
   }
 
-  
-
-  //create a state for list of array,array of strings,
-  //get input from the input box and display on page;
-  //when add task is clicked add input created above.
-  //inside ol, run a for loop to print the list.
-  
-
-  const taskMapper = (value) =>{
-    return <li>{value} </li>
-  }
-
-  return ( 
-    
-    
+  return (
     <div className="container">
       <div className="card">
         <div className="task-input">
-          <input placeholder="enter task" ref = {inputRef} onKeyPress = {keyPressHandler} />
-          <button onClick={addTask}> Add task </button>
+          <input placeholder="enter the task" ref={inputRef} onKeyPress={keyPressHandler} />
+          <button onClick={addTask}> Add a task</button>
         </div>
 
         <div className="task-list">
           <ol>
-            {tasklist.map(taskMapper)}
+            {tasklist.map(listMapper)}
           </ol>
         </div>
       </div>
